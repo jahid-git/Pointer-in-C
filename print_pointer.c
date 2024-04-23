b65bff84ab00a312e786e35e0412056b4a13fd24
root <jahidsite0@gmail.com>
2024-04-23 00:28:09 +0000

    Print and sum x + y by pointer

--- print_pointer.c
+++ print_pointer.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
 int main(){
     int num = 10;
+    int *ptr = &num;
+    
     printf("num = %d\n", num);
     printf("Pointer of num = %p\n", &num);
+    
+    printf("num = %d\n", *ptr);
+    printf("Pointer of num = %p\n", ptr);
+    
+    int x = 20, y = 50, sum;
+    
+    int *ptrX = &x, *ptrY = &y;
+    
+    sum = *ptrX + *ptrY;
+    
+    printf("Sum of x + y = %d\n", sum);
+    
     return 0;
 }
